8e4aa8f3efdae742cb7e06f1a41fa727c837af51
unknown <dfsierra@uninorte.edu.co>
2023-05-05 21:07:09 -0500

    Programa Constelaciones V1

---
+++ Constellations/stars.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+stars = []
+with open('Bot-TelegramApi\Constellations\stars.txt') as file:
+    for line in file:
+        data = line.strip().split()
+        x, y, _, id, mag, harvard, *name = data[:7]
+        stars.append((float(x), float(y), id, float(mag), harvard, name if isinstance(name, list) else name.split(" ")))
+
+x_coords = [star[0] for star in stars]
+y_coords = [star[1] for star in stars]
+plt.scatter(x_coords, y_coords, s=5)
+plt.title("Todas las estrellas")
+plt.show()
